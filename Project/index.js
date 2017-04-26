1288a1063d0727cc6a77a257bb6b8e53a9d4f978
Lukas Vesely <lukas.v@remoteorigin.co>
2017-04-26 19:52:29 +0100

    basic calculating logic (working)

--- Project/index.js
+++ Project/index.js
@@ -1,3 +1,20 @@
-/**
- * Created by PC1 on 26.4.2017.
- */
+window.onload = function () {
+    var numberOfPassengers = document.getElementById("numberOfPassengers");
+var priceOfGas = document.getElementById("priceOfGas");
+var averageConsumption = document.getElementById("averageConsumption");
+var totalDistance = document.getElementById("totalDistance");
+var button = document.getElementById("button");
+
+/*parseInt(numberOfPassengers.value)
+*parseInt(priceOfGas.value)
+*parseInt(averageConsumption.value)
+*parseInt(totalDistance.value)
+* */
+
+function calculate() {
+    alert(((parseInt(totalDistance.value)/100)*parseInt(averageConsumption.value)*parseInt(priceOfGas.value))/parseInt(numberOfPassengers.value) );
+}
+
+button.onclick = calculate;
+
+}
