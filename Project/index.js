cfd5e8211079050bdc8e63c309c8b65b392abe96
Lukas Vesely <lukas.v@remoteorigin.co>
2017-04-26 22:43:00 +0100

    typo changes

--- Project/index.js
+++ Project/index.js
@@ -5,11 +5,12 @@ var averageConsumption = document.getElementById("averageConsumption");
 var totalDistance = document.getElementById("totalDistance");
 var button = document.getElementById("button");
 
-/*parseInt(numberOfPassengers.value)
-*parseInt(priceOfGas.value)
-*parseInt(averageConsumption.value)
-*parseInt(totalDistance.value)
-* */
+/*
+parseInt(numberOfPassengers.value)
+parseInt(priceOfGas.value)
+parseInt(averageConsumption.value)
+parseInt(totalDistance.value)
+*/
 
 function calculate() {
     alert(((parseInt(totalDistance.value)/100)*parseInt(averageConsumption.value)*parseInt(priceOfGas.value))/parseInt(numberOfPassengers.value) );
