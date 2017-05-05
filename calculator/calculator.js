89b07d3f46d5ea657f46360cdd29ffca69a63306
Lukas Vesely <lukas.v@remoteorigin.co>
2017-05-05 17:42:45 +0200

    changes in initialization, add calculate function

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,12 +1,24 @@
 var prompt = require('prompt');
-
 prompt.start();
 
-prompt.get(['numberOfPassengers', 'priceOfGas','averageConsumption','totalDistance'], function (err, result) {
 
-  console.log('Everybody will pay ' + calculate(result.numberOfPassengers,result.priceOfGas,result.averageConsumption,result.totalDistance));
+  prompt.get(['numberOfPassengers', 'priceOfGas', 'averageConsumption', 'totalDistance'], function (err, result) {
 
+    var numberOfPassengers = Number(result.numberOfPassengers);
+    var priceOfGas = Number (result.priceOfGas);
+    var averageConsumption = Number (result.averageConsumption);
+    var totalDistance = Number (result.totalDistance);
+
+
+
+ finalPrice =  calculate(numberOfPassengers, priceOfGas, averageConsumption, totalDistance);
+
+    if (!isNaN(finalPrice))
+    console.log('Everybody will pay ' + finalPrice);
+   else
+      console.log("Your input wasn't valid, please use just numbers ")
 });
+
 
 function calculate (numberOfPassengers,priceOfGas,averageConsumption,totalDistance) {
   return (((parseInt(totalDistance)/100)*parseInt(averageConsumption)*parseInt(priceOfGas))/parseInt(numberOfPassengers))
